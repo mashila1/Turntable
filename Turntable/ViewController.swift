9e84f9518fcfa508c7541cf75d7af49c70c50d4a
mashila1 <mashila1@gmail.com>
2021-01-06 16:05:40 +0800

    加背景還有影片了

--- Turntable/ViewController.swift
+++ Turntable/ViewController.swift
@@ -7,18 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
-    // https://www.itdaan.com/tw/e0b5c42162b6c6d9298fd4e2ef2ba9a5
+    // https://www.youtube.com/watch?v=YcInZDfVYbw
     @IBOutlet weak var wheelImage: WheelImageView!
     @IBOutlet weak var youwantoeatImage: UIImageView!
+    @IBOutlet weak var background: UIImageView!
     @IBOutlet weak var youwantoeatImagechinese: UIImageView!
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         youwantoeatImage.image = UIImage(named: "1.jpg")
         youwantoeatImagechinese.image = UIImage(named: "2.jpg")
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.youtube.com/embed/watch?v=YcInZDfVYbw?loop=1&playlist=YcInZDfVYbw")!
+        let request = URLRequest(url: url)
+        webView.load(request)
         
     }
     @IBAction func clickRotateWheel(_ sender:Any){
